6421b1557d37acc494d6257670d9e87672c1c79d
Elgoh <elgoh3@gmail.com>
2022-10-24 20:21:43 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: "https://ototbackend.com",
+  issuerBaseURL: `https://elgoh.us.auth0.com/`,
+});
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
+// This route doesn't need authentication
+app.get("/api/public", function (req, res) {
+  res.json({
+    message:
+      "Hello from a public endpoint! You don't need to be authenticated to see this.",
+  });
+});
+
+// This route needs authentication
+app.get("/api/private", checkJwt, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated to see this.",
+  });
+});
+
+const checkScopes = requiredScopes("read:current_user");
+
+app.get("/api/private-scoped", checkJwt, checkScopes, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
+  });
+});
+
+app.listen(8393, function () {
+  console.log("Listening on http://localhost:8393");
+});
